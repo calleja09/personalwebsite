dbf6fc8fc418ca3751f9b67f08f352b749be88b4
Arvin Calleja <callejaarvin09@gmail.com>
2024-02-26 21:39:57 +0800

    Personal Website v2 (Tailwind)|Portfolio update.

--- src/components/utils/artsProjects.js
+++ src/components/utils/artsProjects.js
@@ -3,7 +3,7 @@ const ArtsProjects = () => {
         <>
             <div className='container'>
                 <div className='mx-56'>
-                    <h1>Arts</h1>
+                    <h1>In Development.</h1>
                 </div>
             </div>
         </>
